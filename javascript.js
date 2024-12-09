1f056428961e309fabc2239c667b5d9957371ccb
blueberry808 <chowdmia@oregonstate.edu>
2024-12-08 22:45:52 -0800

    changes

--- javascript.js
+++ javascript.js
@@ -5,35 +5,88 @@ const container = document.querySelector("container");
 
 
 //these are children of the container class
-const flex_1 = document.querySelector("flex-container-1");
-const flex_2 = document.querySelector("flex-container-2");
-const flex_3 = document.querySelector("flex-container-3");
-const flex_4 = document.querySelector("flex-container-4");
-
-
-container.appendChild(flex_1); 
-
-
-
-//Need to create a 16x16 array of divs 
-
+const flex_1 = document.querySelector(".flex-container-1");
+const flex_2 = document.querySelector(".flex-container-2");
+const flex_3 = document.querySelector(".flex-container-3");
+const flex_4 = document.querySelector(".flex-container-4");
 
 
 //creating 16x16 array
 
 
+
+
+
 for( let i = 0; i< 4; i++){
-    document.body.appendChild("div");
+
     let newDiv = document.createElement("div"); 
     newDiv.classList.add("subitem")
-    newDiv.style.height = "50px";  
+    newDiv.id = "subitem1" + i;
+    newDiv.style.backgroundColor = "gray";
+    newDiv.style.height = "100px";  
     newDiv.style.width = "50px"; 
-    newDiv.style.border = "5 px solid red"; 
+    newDiv.style.border = "5px solid blue"; 
     newDiv.style.flex = "auto"; 
+
+    newDiv.
+    //newDiv.addEventListener("mouseover", colorFunction(newDiv));
+
 
     flex_1.appendChild(newDiv);  
      
 }
+
+function colorFunction(newDiv)
+{
+    newDiv.style.backgroundColor = "blue";
+}
+
+
+
+
+
+for(let i =0; i<4; i++){
+    console.log("flag 1"); 
+    let newDiv = document.createElement("div"); 
+    newDiv.classList.add("subitem"); 
+    newDiv.style.backgroundColor = "gray"; 
+    newDiv.style.height = "100px";  
+    newDiv.style.width = "50px";
+    newDiv.style.border = "5px solid blue"; 
+    newDiv.style.flex = "auto"; 
+
+    flex_2.appendChild(newDiv); 
+}
+
+for(let i =0; i<4; i++){
+    console.log("flag 1"); 
+    let newDiv = document.createElement("div"); 
+    newDiv.classList.add("subitem"); 
+    newDiv.style.backgroundColor = "gray"; 
+    newDiv.style.height = "100px";  
+    newDiv.style.width = "50px";
+    newDiv.style.border = "5px solid blue"; 
+    newDiv.style.flex = "auto"; 
+
+    flex_3.appendChild(newDiv); 
+}
+for(let i =0; i<4; i++){
+    console.log("flag 1"); 
+    let newDiv = document.createElement("div"); 
+    newDiv.classList.add("subitem"); 
+    newDiv.style.backgroundColor = "gray"; 
+    newDiv.style.height = "100px";  
+    newDiv.style.width = "50px";
+    newDiv.style.border = "5px solid blue"; 
+    newDiv.style.flex = "auto"; 
+
+    flex_4.appendChild(newDiv); 
+}
+
+
+
+
+
 
 
 /*for( let i = 0; i< 4; i++){
@@ -41,7 +94,7 @@ for( let i = 0; i< 4; i++){
     newDiv.classList.add("subitem")
     newDiv.style.height = "50px";  
     newDiv.style.width = "50px"; 
-    newDiv.style.border = "5 px solid red"; 
+    newDiv.style.border = "5px solid red"; 
     newDiv.style.flex = "auto"; 
 
     flex_2.appendChild(newDiv);  
